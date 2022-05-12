ea15b1e7103b8b28ef077de3737e4a9ba9ee2e1c
Thais Alves <alvesthais406@gmail.com>
2022-05-12 17:09:51 -0300

    Parte dinâmica - Dropdown

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ window.addEventListener("load", inicia);
 function inicia() {
     document.getElementById("maisFilmes").addEventListener("click",carregaFilmes);
     document.getElementById("maisAvaliacoes").addEventListener("click",carregaAvaliacoes);
-    document.getElementById("aventura").addEventListener("click",filtraFilmes);
-    document.getElementById("Romance").addEventListener("click",filtraFilmes);
-    document.getElementById("Comedia").addEventListener("click", filtraFilmes);
+
+    document.getElementById("aventura").addEventListener("click",function(){
+        filtraFilmes('a');
+    });
+
+    document.getElementById("Romance").addEventListener("click",function(){
+        filtraFilmes('r');
+    });
+
+    document.getElementById("Comedia").addEventListener("click", function(){
+        filtraFilmes('c');
+    });
 }
 
 function carregaFilmes(){
@@ -16,6 +25,13 @@ function carregaAvaliacoes(){
     //alert("avaliacoes");
 }
 
-function filtraFilmes(){
-    //alert("FiltraFilmes");
+function filtraFilmes(categoria){
+    
+    if(categoria=='a'){
+        document.getElementById("img1").src = "../imagens/passaros.jpg"
+    }else if(categoria == 'r'){
+        document.getElementById("img1").src = "../imagens/genioIndomavel.jpg"
+    }else{
+        document.getElementById("img1").src = "../imagens/pulpFiction.jpg"
+    }
 }
