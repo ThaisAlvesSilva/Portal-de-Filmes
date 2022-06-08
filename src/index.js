3f0fdd5eeedfbde4704225ff852ffb7b2b2bc4e4
Thais Alves <alvesthais406@gmail.com>
2022-06-07 21:31:21 -0300

    Carregando filmes em lançamento

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 $(document).ready(inicia);
 /* http://api.themoviedb.org/3/movie/157336/videos?api_key=5df9d42e52753432b65c92f566de9ae7 */
 var API_KEY = `5df9d42e52753432b65c92f566de9ae7`;
+var movieDB = `https://www.themoviedb.org/movie/`
 var categoria = 't';
 var atualizou;
 var atualizouVideo;
 var imagePath;
-var filmesAPI = [];
 var pos = 0;
-var videos = [];
 var posVideos = 0;
 var dadosDiretores = [];
 var dadosRoteiro = [];
@@ -15,6 +14,8 @@ var dadosRoteiro = [];
 var filmesDrama = [];
 var filmesAnimacao = [];
 var filmesAcao = [];
+var filmesAPI = [];
+var videos = [];
 
 var avaliacoes = [
     {
@@ -41,6 +42,7 @@ function inicia(){
     imagePath = `https://image.tmdb.org/t/p/w500/`; */
 
     getFilmes();
+    carregaLancamentos();
     atualizou = 0;
     atualizouVideo = 0;
     $("#maisFilmes").click(carregaFilmes);
@@ -75,7 +77,8 @@ function getFilmes(){
                 filmesAPI.push(filme);
             })
  
-        }); 
+        });
+         
     URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
     imagePath = `https://image.tmdb.org/t/p/w500/`;
 
@@ -85,10 +88,10 @@ function getFilmes(){
             data.results.map(filme =>{
                 filmesAPI.push(filme);
             })
-
-            
+            console.log(filmesAPI);
         });
-        URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
+
+    URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
     imagePath = `https://image.tmdb.org/t/p/w500/`;
 
     fetch(URL)
@@ -124,19 +127,41 @@ function getFilmes(){
                             if(crew[j].job == "Director" && qtdDiretor != 2){
                                 diretores += crew[j].name + ",";
                             }else if(crew[j].job == "Screenplay" && qtdRoteiro != 2){
-                                roteiro += "," + crew[j].name;
+                                roteiro +=  crew[j].name + "," ;
                             }
                         }
                         dadosDiretores.push(diretores);
                         dadosRoteiro.push(roteiro);
-                            
+                        carregaFilmesVideos();   
                     })
             }
         });
 }
-function atualiza(){
-    location.reload();
+
+function carregaLancamentos(){
+    URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=pt-BR&region=BR`;
+    fetch(URL)
+        .then(res => res.json())
+        .then(filmes => {
+            //console.log(filmes.results);
+           for(var i = 0; i<3; i++){
+                URL = `http://api.themoviedb.org/3/movie/${filmes.results[i].id}/videos?api_key=${API_KEY}&language=pt-BR&region=BR`;
+                var j = 0;
+                fetch(URL)
+                    .then(res => res.json())
+                    .then(v => {
+                        var src = `https://www.youtube.com/embed/${v.results[0].key}`;
+                        var iframe = `#iframeLancamento${j}`;
+                        $(`#linkLancamento${j}`).prop('src', `https://www.themoviedb.org/movie/${filmes.results[i].id}`);
+                        $(iframe).prop('src', src);
+                        $(`#nomeFilmeL${j}`).html("<strong>Nome do filmes:</strong> "+filmes.results[j].title);
+                        $(`#sinopseL${j}`).html("<strong>Sinopse:</strong> "+filmes.results[j].overview);
+                        j++;   
+                    })
+            } 
+        })
 }
+
 function classificaFilmes(){
     for(var i = 0;i<filmesAPI.length;i++){
         for(var j=0; j<filmesAPI[i].genre_ids.length; j++){
@@ -151,35 +176,39 @@ function classificaFilmes(){
     } 
 }
 
-
-
+function atualiza(){
+    location.reload();
+}
 
 function pesquisa(){
     var pesquisa = $('#botaoepesquisa').val();
+
+    if(pesquisa == ""){
+        atualiza();
+    }else{
+        URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=pt-BR&region=BR&query=${pesquisa}`;
+        var filmes = '';
+        fetch(URL)
+            .then(res => res.json())
+            .then(data => {
+                data.results.map(filme => {
+                    if(filme.overview != ""){
+                        if(filme.poster_path != undefined){
+                            filmes += 
+                            `<div style="margin-top:80px; border-color:blue;"class="col-12 col-sm-12 col-md-4 col-lg-4">
+                                <a style="color:black;" href="https://www.themoviedb.org/movie/${filme.id}">
+                                    <img style="width:200px;"src="${imagePath}${filme.poster_path}"</img>
+                                    <p><b> Filme:</b>  ${filme.title}</p>
+                                    <p><b>Descrição:</b> ${filme.overview}</p>
+                                </a>   
+                            </div> `
+                        }
+                    } 
+                })
+                $('#corpoDoSite').html(filmes);
+            });
+    }
     
-    URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=pt-BR&region=BR&query=${pesquisa}`;
-    var filmes = '';
-    fetch(URL)
-        .then(res => res.json())
-        .then(data => {
-            data.results.map(filme => {
-                if(filme.overview != ""){
-                    if(filme.poster_path != undefined){
-                        filmes += 
-                        `<div style="margin-top:80px; border-color:blue;"class="col-12 col-sm-12 col-md-4 col-lg-4">
-                            <a style="color:black;" href="https://www.themoviedb.org/movie/${filme.id}">
-                                <img style="width:200px;"src="${imagePath}${filme.poster_path}"</img>
-                                <p><b> Filme:</b>  ${filme.title}</p>
-                                <p><b>Descrição:</b> ${filme.overview}</p>
-                            </a>   
-                        </div> `
-                    }
-                } 
-            })
-           // console.log(data.results);
-            
-            $('#corpoDoSite').html(filmes);
-        });
 }
 function filtraFilmes(categoria, nomeCategoria){
     atualizou = 0;
@@ -191,27 +220,8 @@ function filtraFilmes(categoria, nomeCategoria){
 }
 
 
-
-
 function carregaFilmes(){
     
-    /* if(atualizou == 0){
-        var pos = 4;
-        atualizou++;
-    }else if(atualizou == 1){
-        var pos = 8;
-        atualizou++;
-    }else if(atualizou == 2){
-        var pos = 12;
-        atualizou++;
-    }else if(atualizou == 3){
-        var pos = 16;
-        atualizou++;
-    }else if(atualizou == 4){
-        var pos = 0;
-        atualizou = 0;
-    } */
-    console.log(categoria);
     if(categoria == 't'){
         if(atualizou == 0){
             pos = 4;
@@ -258,23 +268,26 @@ function carregaFilmes(){
         } 
     }
     
-    console.log(atualizou, pos);
+    //console.log(atualizou, pos);
     alteraFilmes(categoria,pos);
 }
 
-
 function alteraFilmes(categoria, pos){
-    //console.log(filmesAPI);
-    var isCategory = false;
+    
     for(var i=0;i<4;i++, pos++){
+        let link = "#link" + i;
         let img = '#img' + i;
         if(categoria == 'd'){
+            $(link).prop('href', `https://www.themoviedb.org/movie/${filmesDrama[pos].id}`);  
             $(img).prop('src', `${imagePath}${filmesDrama[pos].poster_path}`);  
         }else if(categoria == 'a'){
+            $(link).prop('href', `https://www.themoviedb.org/movie/${filmesAnimacao[pos].id}`);
             $(img).prop('src', `${imagePath}${filmesAnimacao[pos].poster_path}`); 
         }else if(categoria == 'ac'){
+            $(link).prop('href', `https://www.themoviedb.org/movie/${filmesAcao[pos].id}`);
             $(img).prop('src', `${imagePath}${filmesAcao[pos].poster_path}`); 
         }else{  
+            $(link).prop('href', `https://www.themoviedb.org/movie/${filmesAPI[pos].id}`);
             $(img).prop('src', `${imagePath}${filmesAPI[pos].poster_path}`); 
         }
     }         
@@ -293,7 +306,7 @@ function carregaFilmesVideos(){
         atualizouVideo = 0;
     }
     var posV = posVideos;
-    console.log(posVideos);
+    //console.log(posVideos);
     for(var i = 0; i <3;i++, posV++){
         var src = `https://www.youtube.com/embed/${videos[posV].key}`
         $("#iframe" + i).prop('src', src);
